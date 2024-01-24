187f91f15286f21e399a02e41f4111b68eea7139
Willian <brasilsantanawillian1221@gmail.com>
2024-01-24 14:20:25 -0300

    Colocando colas do git e mudando a branch principal para main

--- Git.java
+++ Git.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
-
+/*
+git init [iniciar o git]
+git status
+git log
+git add . ou nome do arquivo
+git commit -m "mensagem"
+git checkout (6 primeiras letras da log ou a branch)
+git branch (nome da branch)
+git branch -m (nome) [renomear nome da branch]
+git merge (nome da branch) [lembrar de voltar para a linha principal]
+*/
 public class Logica{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);

4172237e6bed9d91bc7188293ee95814c080fc5a
unknown <brasilsantanawillian1221@gmail.com>
2024-01-24 10:37:45 -0300

    Somando numeros

--- Logica.java
+++ Logica.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Logica{
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int num1 = input.nextInt();
+        System.out.println("Digite outro numero: ");
+        int num2 = input.nextInt();
+        int soma = num1 + num2;
+        System.out.println("A soma dos numeros é: " + soma);
+        input.close();
     }
 }
